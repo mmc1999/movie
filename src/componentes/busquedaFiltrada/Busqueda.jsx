ebe8d4d400f76d2cb928339a23750268ded9b0f8
mmc1999 <mcentorbi161999@gmail.com>
2022-06-30 16:25:51 -0300

    listo

--- src/componentes/busquedaFiltrada/Busqueda.jsx
+++ src/componentes/busquedaFiltrada/Busqueda.jsx
@@ -15,40 +15,40 @@ const Busqueda = ({handleChange, search,handleSubmit, handleClick, numCheck}) =>
       <div className={BusquedaModule.divRaiting}>
         <p className={BusquedaModule.raiting}>Raiting:</p>
         <div className={BusquedaModule.divStars}>
-          <label>
-            <input type="checkbox" value="1" onChange={handleClick} checked={numCheck >= 1 }/>
+          <label htmlFor="imagen1">
+            <input type="checkbox" value="1" name='imagen1' onChange={handleClick} checked={numCheck >= 1 }/>
             <img 
               src={imagenes} 
               alt='icono de una estrella' 
               className={BusquedaModule.imagenEstrella} 
             />
           </label>
-          <label>
-            <input type="checkbox" value="2" onChange={handleClick} checked={numCheck >= 2 }/>
+          <label htmlFor="imagen2">
+            <input type="checkbox" id="imagen2" value="2" onChange={handleClick} checked={numCheck >= 2 }/>
             <img 
               src={imagenes} 
               alt='icono de una estrella' 
               className={BusquedaModule.imagenEstrella} 
             />
           </label>
-          <label>
-            <input type="checkbox" value="3" onChange={handleClick} checked={numCheck >= 3 }/>
+          <label htmlFor="imagen3">
+            <input type="checkbox" id="imagen3" value="3" onChange={handleClick} checked={numCheck >= 3 }/>
             <img 
               src={imagenes} 
               alt='icono de una estrella' 
               className={BusquedaModule.imagenEstrella} 
             />
           </label>
-          <label>
-            <input type="checkbox" value="4" onChange={handleClick} checked={numCheck >= 4 }/>
+          <label htmlFor="imagen4">
+            <input type="checkbox" id="imagen4" value="4" onChange={handleClick} checked={numCheck >= 4 }/>
             <img 
               src={imagenes} 
               alt='icono de una estrella' 
               className={BusquedaModule.imagenEstrella} 
             />
           </label>
-          <label>
-            <input type="checkbox" value="5" onChange={handleClick} checked={numCheck >= 5 }/>
+          <label htmlFor="imagen5">
+            <input type="checkbox" id="imagen5" value="5" onChange={handleClick} checked={numCheck >= 5 }/>
             <img 
               src={imagenes} 
               alt='icono de una estrella' 
